956d667203790002ef0bb7415aa8e2c726bf7d1d
ezherman <elh605@york.ac.uk>
2022-12-26 08:34:54 +0000

    update snakemake env

--- workflow/installations/snakemake.sh
+++ workflow/installations/snakemake.sh
@@ -1,6 +1,6 @@
 ## TO BE PASTED INTO THE COMMAND LINE
 
-conda create -n snakemake -c conda-forge -c bioconda pandas pyjanitor biopython snakemake mamba pip -y
+conda create -n snakemake -c conda-forge -c bioconda -c bioinf-mcb pandas pyjanitor biopython snakemake mamba pip sra-downloader entrez-direct -y
 
 conda activate snakemake
 pip install wget
